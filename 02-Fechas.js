a8866aa9f32bd92809a1610d66a8e6387c3ec81c
MiniMestri <fonsild97@hotmail.com>
2024-02-03 10:41:22 +0100

    Librerias
    Instanciación de elementos matemáticos, de fechas, etc

---
+++ 02-Fechas.js
@@ -0,0 +1,13 @@
+var fecha = new Date()
+//para conseguir el año
+console.log("El año es: "+fecha.getFullYear())
+//Conseguir el mes
+console.log("El mes es: "+(fecha.getMonth()+1))
+//Conseguir el dia
+console.log("El día es: "+fecha.getDate())
+//Consehior hora
+console.log("La hora es: "+fecha.getHours())
+//Consehior minuto
+console.log("El minuto es: "+fecha.getMinutes())
+//Consehior segundos
+console.log("El segundo es: "+fecha.getSeconds())
