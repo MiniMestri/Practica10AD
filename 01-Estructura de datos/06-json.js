eba22f8ff0b43b91b8b0f9faa865976417559c19
MiniMestri <fonsild97@hotmail.com>
2024-02-03 10:23:19 +0100

    Estructura de datos
    Desarrollo de supuestas colecciones mediante diferentes métodos y técnicas para guardar elementos

---
+++ 01-Estructura de datos/06-json.js
@@ -0,0 +1,20 @@
+var agenda = [
+    {
+        "nombre":"Jose vicente",
+        "telefono":12332145,
+        "email":[
+            "info@josevicentecarratala.com",
+            "info@jocarsa.com",
+            "jocarsa2@gmail.com"
+        ]
+    },
+    {
+        "nombre":"Pepe",
+        "telefono":1233235,
+        "email":[
+            "info@pepe.com",
+
+        ]
+    }
+]
+console.log(agenda)
